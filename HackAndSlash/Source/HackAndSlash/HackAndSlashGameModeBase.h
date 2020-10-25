0eb283140fb83d9c412fae70b42c10de37c84311
Jaewan Park <jaewan.huey.park@gmail.com>
2020-10-25 18:57:23 +0900

    프로젝트 생성

---
+++ HackAndSlash/Source/HackAndSlash/HackAndSlashGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HackAndSlashGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HACKANDSLASH_API AHackAndSlashGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
