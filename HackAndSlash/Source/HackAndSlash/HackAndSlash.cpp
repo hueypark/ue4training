1b12e7428af9043a1b1095f53015f0a915eea8a5
Jaewan Park <jaewan.huey.park@gmail.com>
2020-10-25 18:54:22 +0900

    템플릿 프로젝트 삭제
    너무 많은 기능을 템플릿 프로젝트에서 제공해주어서 동작을 이해하기 힘들었음

--- HackAndSlash/Source/HackAndSlash/HackAndSlash.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "HackAndSlash.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HackAndSlash, "HackAndSlash" );
-
-DEFINE_LOG_CATEGORY(LogHackAndSlash)
- 
